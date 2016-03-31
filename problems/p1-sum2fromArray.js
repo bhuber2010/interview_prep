9d28a319de8351c171275ae16f9c7e12f59d737c
bhuber2010 <bhuber@alumni.colostate.edu>
2016-03-31 13:53:53 -0600

    basically working, need to continue expanding func

--- problems/p1-sum2fromArray.js
+++ problems/p1-sum2fromArray.js
@@ -1,20 +1,34 @@
 
 export function sumArray(array, expectedSum) {
-  var values = false;
+  var values = {
+    sum: false,
+    subtract: [],
+    multiply: false
+  };
   var isPossible = array.reduce((result, num, i) => {
     var testArray = array.slice(i);
     testArray.splice(testArray.indexOf(num), 1);
     var currentValue = false;
     if (testArray.length !== 0) {
       currentValue = testArray.reduce((result2, num2, j) => {
-        // console.log(num, num2, num+num2);
         if (num + num2 === expectedSum) {
-          values = `${num} + ${num2} = ${expectedSum}`;
-          return true;
+          values.sum = `${num} + ${num2} = ${expectedSum}`;
+        };
+        if (num * num2 === expectedSum) {
+          values.multiply = `${num} * ${num2} = ${expectedSum}`;
+        };
+        if (num - num2 === expectedSum) {
+          values.subtract.push(`${num} - ${num2} = ${expectedSum}`);
+        };
+        if (num2 - num === expectedSum) {
+          values.subtract.push(`${num2} - ${num} = ${expectedSum}`);
+        };
+        if (values.sum || values.multiply || values.subtract.length !== 0) {
+          return true
         } else {
           return result2 === true ? true : false;
         }
-      }, testArray[0])
+      }, false)
     }
     return result === true ? true : currentValue;
   }, false)
--- test/spec/p1-sum2fromArray.js
+++ test/spec/p1-sum2fromArray.js
@@ -6,26 +6,32 @@ export function p1() {
   'use strict';
 
 
-  describe('P1 - Sum two numbers from array to equal desired result', function () {
+  describe('P1 - Add, Subtract, or Multiply two numbers from array to equal desired result', function () {
 
     describe('cases when it should return false', function () {
-      it('should return false as sum is impossible', function () {
+      it('should return false as all operations are impossible', function () {
         sumArray([1,2,3], 10).should.equal(false);
       });
-      it('should return false, there is no combination', function(){
-        sumArray([6, 4, 9, 2, 1], 4).should.equal(false);
-      })
     });
     describe('cases when it should return the two numbers that sum to the result (or true)', function(){
       it('should return true, 1 + 3 = 4', function(){
-        sumArray([1,2,3], 4).should.equal("1 + 3 = 4");
+        var test2 = sumArray([1,2,3], 4);
+        test2.sum.should.equal("1 + 3 = 4");
+        test2.subtract.length.should.equal(0);
+        test2.multiply.should.equal(false);
       });
       it('should return true, 4 + 1 = 5', function(){
-        sumArray([6, 4, 9, 2, 1], 5).should.equal("4 + 1 = 5");
+        var test3 = sumArray([6, 4, 9, 2, 1], 5);
+        test3.sum.should.equal("4 + 1 = 5");
+        test3.subtract.should.equal(["6 - 1 = 5", "9 - 4 = 5"]);
+        test3.multiply.should.equal(false);
       });
-      it('should return true, 2 + 2 = 4', function(){
+      xit('should return true, 2 + 2 = 4', function(){
         sumArray([2, 4, 9, 2, 1], 4).should.equal("2 + 2 = 4");
       });
+      it('should return true, 6 - 2 = 4', function(){
+        sumArray([6, 4, 9, 2, 1], 4).subtract.should.equal(["6 - 2 = 4"]);
+      })
     });
   });
 }
