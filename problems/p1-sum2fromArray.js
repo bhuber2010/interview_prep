220ca4ad2153c61e194639f187fbb3b617d53a87
bhuber2010 <bhuber@alumni.colostate.edu>
2016-03-31 14:35:17 -0600

    refector, all tests passing for +, -, *

--- problems/p1-sum2fromArray.js
+++ problems/p1-sum2fromArray.js
@@ -1,9 +1,9 @@
 
 export function sumArray(array, expectedSum) {
   var values = {
-    sum: false,
+    sum: [],
     subtract: [],
-    multiply: false
+    multiply: []
   };
   var isPossible = array.reduce((result, num, i) => {
     var testArray = array.slice(i);
@@ -12,10 +12,10 @@ export function sumArray(array, expectedSum) {
     if (testArray.length !== 0) {
       currentValue = testArray.reduce((result2, num2, j) => {
         if (num + num2 === expectedSum) {
-          values.sum = `${num} + ${num2} = ${expectedSum}`;
+          values.sum.push(`${num} + ${num2} = ${expectedSum}`);
         };
         if (num * num2 === expectedSum) {
-          values.multiply = `${num} * ${num2} = ${expectedSum}`;
+          values.multiply.push(`${num} * ${num2} = ${expectedSum}`);
         };
         if (num - num2 === expectedSum) {
           values.subtract.push(`${num} - ${num2} = ${expectedSum}`);
@@ -23,7 +23,7 @@ export function sumArray(array, expectedSum) {
         if (num2 - num === expectedSum) {
           values.subtract.push(`${num2} - ${num} = ${expectedSum}`);
         };
-        if (values.sum || values.multiply || values.subtract.length !== 0) {
+        if (values.sum.length !== 0 || values.multiply.length !== 0 || values.subtract.length !== 0) {
           return true
         } else {
           return result2 === true ? true : false;
--- test/spec/p1-sum2fromArray.js
+++ test/spec/p1-sum2fromArray.js
@@ -16,21 +16,32 @@ export function p1() {
     describe('cases when it should return the two numbers that sum to the result (or true)', function(){
       it('should return true, 1 + 3 = 4', function(){
         var test2 = sumArray([1,2,3], 4);
-        test2.sum.should.equal("1 + 3 = 4");
+        test2.sum.should.have.length(1);
+        test2.sum[0].should.equal("1 + 3 = 4");
         test2.subtract.length.should.equal(0);
-        test2.multiply.should.equal(false);
+        test2.multiply.should.have.length(0);
       });
       it('should return true, 4 + 1 = 5', function(){
         var test3 = sumArray([6, 4, 9, 2, 1], 5);
-        test3.sum.should.equal("4 + 1 = 5");
-        test3.subtract.should.equal(["6 - 1 = 5", "9 - 4 = 5"]);
-        test3.multiply.should.equal(false);
+        test3.sum.should.have.length(1);
+        test3.sum[0].should.equal("4 + 1 = 5");
+        test3.subtract.should.have.length(2);
+        test3.subtract[0].should.equal("6 - 1 = 5");
+        test3.subtract[1].should.equal("9 - 4 = 5");
+        test3.multiply.length.should.equal(0);
       });
-      xit('should return true, 2 + 2 = 4', function(){
-        sumArray([2, 4, 9, 2, 1], 4).should.equal("2 + 2 = 4");
+      it('should return true, 2 + 2 = 4', function(){
+        var test4 = sumArray([2, 4, 9, 2, 1], 4);
+        test4.sum.should.have.length(1);
+        test4.sum[0].should.equal("2 + 2 = 4");
       });
       it('should return true, 6 - 2 = 4', function(){
-        sumArray([6, 4, 9, 2, 1], 4).subtract.should.equal(["6 - 2 = 4"]);
+        var test5 = sumArray([6, 4, 9, 2, 1], 4);
+        test5.sum.length.should.equal(0);
+        test5.subtract.should.have.length(1);
+        test5.subtract[0].should.equal("6 - 2 = 4");
+        test5.multiply.length.should.equal(1);
+        test5.multiply[0].should.equal("4 * 1 = 4");
       })
     });
   });
