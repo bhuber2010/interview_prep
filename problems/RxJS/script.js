ceaa7967f0c53c3fc34e59a3b9d09bd13891b98c
bhuber2010 <bhuber@alumni.colostate.edu>
2016-07-06 13:33:10 -0700

    finish up RxJS example w/ single item refresh

--- problems/RxJS/script.js
+++ problems/RxJS/script.js
@@ -1,31 +1,49 @@
 
 var refreshButton = document.querySelector(".refresh");
+var closeButtonsArray = [...document.querySelectorAll('.close')];
 
 var sartupRequestStream = Rx.Observable.just("https://api.github.com/users");
 
 var refreshClickStream = Rx.Observable.fromEvent(refreshButton, 'click');
 
-var requestStream = refreshClickStream.map(event => {
-  var randomOffset = Math.floor(Math.random() * 500);
-  return "https://api.github.com/users?since=" + randomOffset;
+var closeClickStreamsArray = closeButtonsArray.map(button => {
+  return Rx.Observable.fromEvent(button, 'click');
 })
 
+var requestStream = createRequestStream(refreshClickStream);
 
-var responseStream = requestStream.merge(sartupRequestStream)
-  .flatMap(requestUrl => {
-    return Rx.Observable.fromPromise($.getJSON(requestUrl))
-  })
+var responseStream = createResponseStream(requestStream, sartupRequestStream);
+
+var closeResponseStreamsArray = closeClickStreamsArray.map(stream => {
+  return createResponseStream(createRequestStream(stream));
+})
 
 // responseStream.subscribe(response => {
 //   console.log(response);
 // })
 
-function createSuggestionStream(responseStream) {
-  return responseStream.map(listUser => {
+function createRequestStream(eventStream) {
+  return eventStream.map(event => {
+    var randomOffset = Math.floor(Math.random() * 500);
+    return "https://api.github.com/users?since=" + randomOffset;
+  })
+}
+
+function createResponseStream(inputStream, startStream) {
+  var stream = startStream ? inputStream.merge(sartupRequestStream) : inputStream;
+  return stream.flatMap(requestUrl => {
+    console.log(requestUrl);
+    return Rx.Observable.fromPromise($.getJSON(requestUrl))
+  }).shareReplay(1)
+}
+
+function createSuggestionStream(responseStream, closeResponseStream) {
+  var stream = responseStream.merge(closeResponseStream);
+  return stream.map(listUser => {
     return listUser[Math.floor(Math.random() * listUser.length)]
   })
   .startWith(null)
-  .merge(refreshClickStream.map(ev => null));
+  // .merge(refreshClickStream.map(ev => null)); // This line clears last users immediatly on refresh click
 }
 
 function renderSuggestion(userData, element) {
@@ -41,8 +59,8 @@ function renderSuggestion(userData, element) {
   }
 }
 
-function createAndRenderStream(responseStream, element) {
-  var stream = createSuggestionStream(responseStream);
+function createAndRenderStream(responseStream, closeResponseStream, element) {
+  var stream = createSuggestionStream(responseStream, closeResponseStream);
   suggestionStreams.push(stream);
   stream.subscribe(user => {
     renderSuggestion(user, element);
@@ -52,6 +70,6 @@ function createAndRenderStream(responseStream, element) {
 var suggestionStreams = [];
 
 var suggestionElements = document.querySelectorAll('.suggestion');
-suggestionElements.forEach(suggestionEl => {
-  createAndRenderStream(responseStream, suggestionEl)
+suggestionElements.forEach((suggestionEl, i) => {
+  createAndRenderStream(responseStream, closeResponseStreamsArray[i], suggestionEl)
 })
